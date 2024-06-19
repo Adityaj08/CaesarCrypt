32d43270211dddd348713355fcac6a6ed3622fe7
Adityaj08 <adityaavadhootjoshi@gmail.com>
2024-06-19 22:26:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from cipher import Cipher
+import pyperclip
+
+# Streamlit App
+def main():
+    st.title("Cipher Encryption and Decryption")
+    cipher = Cipher()
+    st.sidebar.title("Cipher Options")
+    choice = st.sidebar.selectbox("Choose the Mode", ["Encrypt", "Decrypt"],label_visibility="collapsed",index=None) or "Encrypt"
+    st.sidebar.markdown("---")
+    if st.sidebar.button("sign-in",use_container_width=True):
+        st.sidebar.info("To be Added soon...",icon=":material/info:")
+    st.sidebar.link_button("Github >","",use_container_width=True,type="primary")
+
+    if choice == "Encrypt":
+        st.subheader("Encryption")
+        message = st.text_input("Enter the message to encrypt",placeholder="Ex: Secret Message")
+        key = st.number_input("Enter the Key", min_value=0, step=1,placeholder="Ex: 11")
+        st.info("Pressing Encrypt will automatically copy encrypted text to clipboard",icon=":material/info:")
+        if st.button("Encrypt",use_container_width=True,type="primary"):
+            if message:
+                encrypted_message = cipher.encrypt(message, key)
+                st.text_area(f"Encrypted text", value=encrypted_message)
+                pyperclip.copy(encrypted_message)
+                st.toast("Encrypted text copied to clipboard!",icon=":material/content_paste:")
+            else:
+                st.toast(":red[Please enter a message to encrypt!]",icon=":material/error:")
+
+    elif choice == "Decrypt":
+        st.subheader("Decryption")
+        message = st.text_input("Enter the message to decrypt",placeholder="Ex: Encrypted Message")
+        key = st.number_input("Enter the Key", min_value=0, step=1)
+        if st.button("Decrypt",use_container_width=True,type="primary"):
+            if message:
+                decrypted_message = cipher.decrypt(message, key)
+                st.text_area(f"Decrypted Text:",value=decrypted_message)          
+            else:
+                st.toast(":red[Please enter a message to decrypt!]",icon=":material/error:")
+
+
+if __name__ == '__main__':
+    main()
---
+++ cipher.py
@@ -0,0 +1,65 @@
+class Cipher():
+
+    def __init__(self):
+        self.LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'*100
+        
+
+    def __crypt(self, mode):
+        translated = ''
+        for symbol in self.message.upper():
+            if symbol in self.LETTERS:
+                num = self.LETTERS.find(symbol)
+                if mode == 'encrypt':
+                    num = num + self.key
+                elif mode == 'decrypt':
+                    num = num - self.key
+
+                if num >= len(self.LETTERS):
+                    num = num - len(self.LETTERS)
+                elif num < 0:
+                    num = num + len(self.LETTERS)
+
+                translated = translated + self.LETTERS[num]
+            else:
+                translated = translated + symbol
+
+        return translated
+
+    def encrypt(self, message, key=0):
+        self.translated = ''
+        self.key = key
+        self.message = message
+        return self.__crypt('encrypt')
+
+    def decrypt(self, message, key=0):
+        self.translated = ''
+        self.key = key
+        self.message = message
+        return self.__crypt('decrypt')
+
+if __name__ == '__main__':
+    cipher = Cipher()
+    while True:
+        try:
+            choice = int(input("Choose The Mode\n\t1) encrypt\n\t2) Decrypt\n\t3) Exit\nEnter Choice: "))
+        except ValueError:
+            print("Error : Please enter integer (1, 2, 3)")
+        match choice:
+            case 1:
+                message = input("Enter the message to encrypt: ")
+                try:
+                    key = int(input("Enter the Key: "))
+                except ValueError:
+                    print("Error : Please enter integer")
+                print(f"\nThe encrypted Text is \n\"{cipher.encrypt(message, key)}\"")
+            case 2:
+                message = input("Enter the message to decrypt: ")
+                try:
+                    key = int(input("Enter the Key: "))
+                except ValueError:
+                    print("Error : Please enter integer")
+                print(f"\nThe Decrypted Text is \n\"{cipher.decrypt(message, key)}\"")
+            case 3:
+                exit(0)
+            case _:
+                print("Enter a valid choice: ") 
