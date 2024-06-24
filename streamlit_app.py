02a8edcb08b1fb8105718ef054cc3d5d63070811
Adityaj08 <adityaavadhootjoshi@gmail.com>
2024-06-24 15:55:17 +0530

    Update streamlit_app.py to use more accessible copy to clipboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from cipher import Cipher
-import pyperclip
 
 # Streamlit App
 def main():
@@ -18,12 +17,12 @@ def main():
         st.subheader("Encryption")
         message = st.text_input("Enter the message to encrypt",placeholder="Ex: Secret Message")
         key = st.number_input("Enter the Key", min_value=0, step=1,placeholder="Ex: 11")
-        st.info("Pressing Encrypt will automatically copy encrypted text to clipboard",icon=":material/info:")
+        st.info("Pressing Encrypt will automatically enable copy to clipboard option",icon=":material/info:")
         if st.button("Encrypt",use_container_width=True,type="primary"):
             if message:
                 encrypted_message = cipher.encrypt(message, key)
-                st.text_area(f"Encrypted text", value=encrypted_message)
-                pyperclip.copy(encrypted_message)
+                st.write("Encrypted text")
+                st.code(encrypted_message,language="str")
                 st.toast("Encrypted text copied to clipboard!",icon=":material/content_paste:")
             else:
                 st.toast(":red[Please enter a message to encrypt!]",icon=":material/error:")
@@ -35,7 +34,8 @@ def main():
         if st.button("Decrypt",use_container_width=True,type="primary"):
             if message:
                 decrypted_message = cipher.decrypt(message, key)
-                st.text_area(f"Decrypted Text:",value=decrypted_message)          
+                st.write("Decrypted text")
+                st.code(decrypted_message,language="str")      
             else:
                 st.toast(":red[Please enter a message to decrypt!]",icon=":material/error:")
 
